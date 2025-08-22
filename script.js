0680a4d1dba9c55ec8173afec2a2447285551519
pikachu5012 <https://github.com/pikachu5012.git>
2025-08-22 16:33:44 +0300

    adding test section and responsive

--- script.js
+++ script.js
@@ -81,3 +81,34 @@ window.onload = function() {
     animateCounter("counter2", 1, 37, 1500);
     animateCounter("counter3", 1, 15, 1500);
 };
+
+//test section
+let cardContainer = document.querySelector(".card-container");
+let cards = document.querySelectorAll(".card");
+let offset = 0;
+
+const step = 380; 
+const minTranslate = -(cards.length * 350 - cardContainer.offsetWidth/1.25); // max left
+const maxTranslate = 0; 
+
+function moveItems(direction) {
+  offset += direction * step;
+
+  // Clamp between min and max
+  if (offset < minTranslate) offset = minTranslate;
+  if (offset > maxTranslate) offset = maxTranslate;
+
+  // Apply translate to all items
+  cards.forEach((item) => {
+    item.style.transform = `translateX(${offset}px)`;
+  });
+}
+
+function slider(type) {
+  if (type === "right") {
+    moveItems(-1);
+  }
+  if (type === "left") {
+    moveItems(1);
+  }
+}
